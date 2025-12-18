fe0818e0108a6ecb63c38904238031af2a06cc71
MorenoCLR <github@morenoclr.com>
2025-12-18 16:53:22 +0700

    fix: grant execute permission to authenticated users for delete_user RPC

--- db/migrations/0007_add_delete_user_function.sql
+++ db/migrations/0007_add_delete_user_function.sql
@@ -3,8 +3,12 @@ CREATE OR REPLACE FUNCTION delete_user()
 RETURNS void
 LANGUAGE plpgsql
 SECURITY DEFINER
+SET search_path = public
 AS $$
 BEGIN
   DELETE FROM auth.users WHERE id = auth.uid();
 END;
 $$;
+
+REVOKE EXECUTE ON FUNCTION delete_user() FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION delete_user() TO authenticated;
